05ff02848d35d46edd96a3dd8677bcf5246c17e4
George <george.bobovsky@gmail.com>
2025-08-29 20:45:07 +0500

    dishesSlice replaced with dishesApi

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { dishesSlice } from "./features/dishes/slice";
 import { cartSlice } from "./features/cart/slice";
 import { requestsSlice } from "./features/requests/slice";
 import { reviewsApi } from "./services/reviews/api";
@@ -7,15 +6,16 @@ import { usersApi } from "./services/users/api";
 import { restaurantsApi } from "./services/restaurants/api";
 import { imagesSlice } from "./features/images/slice";
 import { restaurantsWithImageSlice } from "./features/restaurantsWithImage/slice";
+import { dishesApi } from "./services/dishes/api";
 
 export const store = configureStore({
   reducer: {
-    [dishesSlice.name]: dishesSlice.reducer,
     [cartSlice.name]: cartSlice.reducer,
     [imagesSlice.name]: imagesSlice.reducer,
     [restaurantsWithImageSlice.name]: restaurantsWithImageSlice.reducer,
     [requestsSlice.name]: requestsSlice.reducer,
     [restaurantsApi.reducerPath]: restaurantsApi.reducer,
+    [dishesApi.reducerPath]: dishesApi.reducer,
     [reviewsApi.reducerPath]: reviewsApi.reducer,
     [usersApi.reducerPath]: usersApi.reducer,
   },
@@ -27,6 +27,7 @@ export const store = configureStore({
       serializableCheck: false,
     }).concat(
       restaurantsApi.middleware,
+      dishesApi.middleware,
       reviewsApi.middleware,
       usersApi.middleware
     ),
